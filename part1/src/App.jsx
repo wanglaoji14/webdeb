95a322829ceea422cc645a22a2b3de91e98431ef
ZHU <zhucy1208@gmail.com>
2026-07-26 17:28:10 +0900

    feat: complete exercise 1.1 and 1.2, cleanup template files

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,52 @@
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
 
-  return (
-    <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </div>
+  const Header = (props) =>{
+    return (
+    <h1>{props.course}</h1>
+  ) 
+  }
+  const Content = (props) =>{
+    return (
+      <div>
+        <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+      </div>
+      
+    )
+  }
+
+  const Total = (props) =>{
+    return (
+    <p>Number of exercises {props.Total}</p>
   )
-}
+  }
 
-export default App
+  const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+      <div>
+        < Header course = {course} />
+        < Content
+          part1 = {part1} exercises1 = {exercises1}
+          part2 = {part2} exercises2 = {exercises2}
+          part3 = {part3} exercises3 = {exercises3} 
+         />
+        < Total Total = {exercises1 + exercises2 + exercises3} />
+      </div>
+    )
+  }
+
+  export default App
